3f218f0ee3c8257bf33e919d0a1c722bc66b00e7
Tara Habersack <taraleehabersack@gmail.com>
2015-02-18 20:42:26 -0800

    present not working now, in class

--- ex47/engine.py
+++ ex47/engine.py
@@ -2,29 +2,42 @@ from tile_desc import scene
 from graph import graph
 from encounters import encounter
 
-direction = raw_input("direction?  >")
+user = ["go to sleep north run away"]
+
+directions = {'north': -6, 'east': 1, 
+	      'south': 6, 'west': -1
+	     }		    
+actions = {'sleep': +3, 'open bag': 0, 
+	   'sing': +1
+	  }
+directory = {0: directions, 1: actions}
 
 class Engine(object):
-    var = 'o'
-  
-    directions = {'north': 1, 'east': 2,
-		  'south': 3, 'west': 4
-		 }
-		    
-    actions = ['sleep', 'open bag', 'sing']
     
-    def go(self, current_space):
-        print Engine.directions
-        map(direction, Engine.directions)
+    def __init__(self, current_space):
+        self.current_space = current_space
+        
+    def scene(self, current_space):
+        print scene[int(current_space)]
+        self.encounter(current_space)
+        
+    def encounter(self, current_space):
+        print "at encounter"
+        self.incoming(current_space)
+        
+    def incoming(self, current_space):
+        print "at incoming"
+        user = raw_input(["direction?>  "])
+        print user
     
-#for each object in direction check each object in directions
- 
-
-        
-        
-current_space = 16
-test = Engine()
-test.go(current_space)     
 
 
-Engine.directions[direction]
+
+
+
+
+	    
+current_space = 5
+
+go = Engine(current_space)
+go.scene(current_space)
