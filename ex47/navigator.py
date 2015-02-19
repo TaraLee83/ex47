f4476999c0549b42f88c70f03aa15c01e26a4350
Tara Habersack <taraleehabersack@gmail.com>
2015-02-18 19:44:58 -0800

    move tester working

--- ex47/navigator.py
+++ ex47/navigator.py
@@ -13,17 +13,15 @@ directory = {0: directions, 1: actions}
 
 
 def test_move(self, token, current_space):
-    print token
-    print current_space
     step = directions[token]
-    print step
-    if step in graph[current_space]:
-        print "success"
+    possible = step + current_space
+    if possible in graph[current_space]:
+        print "You can go this way."
     else:
         print "You can't go this way."
 
     
 current_space = 12    
-token = 'north'
+token = 'south'
 go = test_move('stuff', token, current_space)
 	       
