d23e4df061b2ebdb6940b58c9784e0f5d6c45d7b
Tara Habersack <taraleehabersack@gmail.com>
2015-02-27 14:54:03 -0800

    Add glasssmith

--- ex47/tile_desc.py
+++ ex47/tile_desc.py
@@ -24,23 +24,40 @@ scene = {1: 'meadow desc',
          22: 'forest desc',
          23: 'forest desc',
          24: 'forest desc',
-         25: 'desert desc',
+         25: """What had seemed a mirage was now clearly a hut.
+	     It's walls were of roughly crafted glass, clear
+	     and as deep as the length of your arm and therefore
+	     opaque. You made out what could be a table and in
+	     the opposite corner the flicker of a fatted wick.
+	     Though the light indicated a presence nothing 
+	     within looked to be the shape of a . . . wait!
+	     Something had moved ever so slightly. There, hunched
+	     over an oversized table a figure was hard at work
+	     on something. Might they mind an interrupption? 
+	     Might they mind it enough to use whatever fantastic
+	     weapons a so terribly talented glasssmith could 
+	     produce.
+	     """,
          26: 'desert desc',
          27: 'desert desc',
          28: 'forest desc',
          29: 'entrance to Garzhed',
          30: 'Two possible encounters, a healer and the blacksmith',
-         31: 'desert desc',
+         31: """Hills of green and mustard-yellow glass-shards
+	     stretched from the North-West, cave riddled rock-face
+	     to the North-East, where they abruptly curved due 
+	     North.
+	     """
          32: 'desert desc',
          33: 'desert desc',
          34: 'landing rock',
          35: 'forest desc',
          36: """Over the impenetrable trees to the North you 
-	      noticed many separate plumes of smoke. You heard 
-	      metal striking metal, sawing, and shouting and 
-	      laughing. One winded a lute to the North-east 
-	      and a mistrel sung and strummed to the North-west.
-	      """
+	     noticed many separate plumes of smoke. You heard 
+	     metal striking metal, sawing, and shouting and 
+	     laughing. One winded a lute to the North-east 
+	     and a mistrel sung and strummed to the North-west.
+	     """
 	}   
 
 
