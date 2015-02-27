02e55b1bd5dc6146307b76cdea2280efda81eb93
Tara Habersack <taraleehabersack@gmail.com>
2015-02-27 13:42:21 -0800

    remove exception.py

--- ex47/exception.py
+++
@@ -1,1 +0,0 @@
-#I'm going to try to make an exception class that will handle the issue of needing to know wether or not a subkey was found in engine.Engine.waldo and then acting on either possibility separately
