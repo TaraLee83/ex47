b62c8292e8ac307635dc2493919a5eb2ea9aa3f2
Tara Habersack <taraleehabersack@gmail.com>
2015-03-04 14:58:44 -0800

    Update

--- ex47/attack_workshop.py
+++ ex47/attack_workshop.py
@@ -7,22 +7,26 @@ hero_hp = [65]
 
 def dragon_attack(self, current_space):
     dragon_chooser = random.choice(Dragons)
+    print dragon_chooser
     group = random.choice(dragon_chooser[1:5])
     pull_val = group[0:1] 
-    pull_desc = group[1:2]
-    desc = (", ".join(repr(e) for e in pull_desc))
+    pull_desc_a = group[1:2]
+    pull_desc_b = group[2:3]
+    intro = (", ".join(repr(e) for e in pull_desc_a))
+    conclusion = (", ".join(repr(e) for e in pull_desc_b))
     roll = random.randint(0, 4)
-    print desc
+    print intro
     if roll == 1:
         print "You're lucky!"		    
     elif roll != 1:
+        print conclusion
         health = sum(hero_hp)
         change = sum(pull_val)
         new_val = health + change
         print "Your health went from %r to %r." % (health, new_val)
         del hero_hp[:]
         hero_hp.append(new_val)
-        print hero_hp
+
         
         
 def damage(self, current_space, dict, a, b):
