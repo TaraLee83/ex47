62827319675ce4080f9921a2f3a9b811992db123
Tara Habersack <taraleehabersack@gmail.com>
2015-03-04 13:31:53 -0800

    HP update added

--- ex47/attack_workshop.py
+++ ex47/attack_workshop.py
@@ -1,21 +1,28 @@
 ###pull from attacks dict randomly, print desc of attack, use value to calculate hp loss. If hero dead end game, if hero lives pass.
 from foe_hitpoints import RedDragon
 import random
+from library import Dragons
 
-
+hero_hp = [65]
 
 def dragon_attack(self, current_space):
     dragon_chooser = random.choice(Dragons)
     group = random.choice(dragon_chooser[1:5])
     pull_val = group[0:1] 
-    value = (", ".join(repr(e) for e in pull_val))
     pull_desc = group[1:2]
     desc = (", ".join(repr(e) for e in pull_desc))
     roll = random.randint(0, 4)
+    print desc
     if roll == 1:
         print "You're lucky!"		    
     elif roll != 1:
-        print desc
+        health = sum(hero_hp)
+        change = sum(pull_val)
+        new_val = health + change
+        print "Your health went from %r to %r." % (health, new_val)
+        del hero_hp[:]
+        hero_hp.append(new_val)
+        print hero_hp
         
         
 def damage(self, current_space, dict, a, b):
