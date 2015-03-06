1c46579ba33a2033c94a73c83443302e1695d503
Tara Habersack <taraleehabersack@gmail.com>
2015-03-06 13:56:25 -0800

    Testing pure text output format option

---
+++ ex47/text.py
@@ -0,0 +1,43 @@
+#Dragons
+#Red
+red_intro = """From the mouth of the volcano! Raaaawwwrr!"""
+red_claws_before = """red_claws_before"""
+red_claws_after = """red_claws_after"""
+red_fire_breath_before = """Red fire_breath before"""
+red_fire_breath_after = """Red fire_breath after"""
+red_finisher_before = """Red finisher before"""
+red_finisher_after = """Red finisher after"""
+#Green
+green_intro = """The spruce trees blanket the lowest part of the Eastern snow-capped
+      	    mountain range, their frequency becomes spotted closer to histears
+      	    river. The trees that dot the shoreline are thick and burled, the
+      	    waterfront side stripped of all branches and leaves and enough bark
+      	    to make the snaking river appear to have stripes of white and green
+      	    flanking. It is a disruption in this bone white and deep green striping
+      	    that first catches your eye. Soon after, grey smoke effluent of nostrils
+      	    and bone white just like the trees, eyes, amber afire, seeming to burn
+      	    from within and the bleached-bone teeth that shaped her mouth into 
+      	    a wicked grin, you see now that Dragons can smile. Perhaps this will be
+      	    the last thing you ever learn.
+      	    """
+green_claw_attack_before = """Green claw attack before"""
+green_claw_attack_after = """Green claw attack after"""
+green_tail_swipe_before = """Green tail_swipe before"""
+green_tail_swipe_after = """Green tail_swipe after"""
+green_fire_breath_before = """Green fire_breath before"""
+green_fire_breath_after = """Green fire_breath after"""
+green_finisher_before = """Green finisher before"""
+green_finisher_after = """Green finisher after"""
+#Gold
+gold_intro = """What could have been the sun itself, lain upon the mountainside, shifted. 
+	   Diamond-like eyes opened in its' head, that larger than the mouth of the 
+	   river.
+	   """
+gold_claws_before = """gold_claws_before"""	   
+gold claws_after = """gold claws after"""
+gold_tail_swipe before = """Gold tail_swipe before"""
+gold_tail_swipe after = """Gold tail_swipe after"""
+gold_fire_breath before = """Gold fire_breath before"""
+gold_fire_breath after = """Gold fire_breath after"""
+gold_finisher_before = """Gold finisher before"""
+gold_finisher_after = """Gold finisher after"""
