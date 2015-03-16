2f0c28768eca7b32928a16af23b32fbae2a50da9
Tara Habersack <taraleehabersack@gmail.com>
2015-03-15 21:02:10 -0700

    Update

--- ex47/encounters.py
+++ ex47/encounters.py
@@ -3,11 +3,6 @@
 import random
 from graph import encounter
 
-direction = {'north': -6, 'east': 1, 
-	      'south': 6, 'west': -1
-	     }		    
-action = {'sleep': +3, 'open bag': 0, 
-	   'sing': +1
 	  }
 directory = {0: direction, 1: action, 2:encounter}
 
@@ -17,7 +12,12 @@ def roll(self, current_space):
     if x == y:
         go = map_encounter('stuff', current_space)
     else:
-        print x,"no"
+        print x,"n
+direction = {'north': -6, 'east': 1, 
+	      'south': 6, 'west': -1
+	     }		    
+action = {'sleep': +3, 'open bag': 0, 
+	   'sing': +1o"
         
 def map_encounter(self, current_space):
     x = encounter[current_space]
@@ -29,32 +29,7 @@ def map_encounter(self, current_space):
         pass
     elif 'Forest' in x:
         pass
-    
-def dragon_chooser(self, current_space):
-    dragon = random.randint(0, 2)
-    if dragon == 0:
-        red_dragon('stuff', current_space)
-    elif dragon == 1:
-        green_dragon('stuff', current_space)
-    elif dragon == 2:
-        gold_dragon('stuff', current_space)
-        
-def red_dragon(self, current_space):
-    attack = ("go", "stay", "inbetween")
-    print random.choice(attack)
-    if sum(hero_hitpoints.py 
-    
-    
-def green_dragon(self, current_space):
-    print "green_dragon"
-    attack = ("go", "stay", "inbetween")
-    print random.choice(attack)
-    
-def gold_dragon(self, current_space):
-    print "gold_dragon"
-    attack = ("go", "stay", "inbetween")
-    print random.choice(attack)
-    
+
     
         
 current_space = 10
