b67ff9040eaf9ab526b24ba358ac4cf8762890b9
Tara Habersack <taraleehabersack@gmail.com>
2015-04-04 14:18:33 -0700

    Condensed attack workshops

--- ex47/attack_workshop_1.py
+++ ex47/attack_workshop_1.py
@@ -2,9 +2,8 @@
 import random
 import textwrap
 from sys import exit
-from library import Dragons, Hit_Points, encounter
+from library import Dragons, Scorpians, Hit_Points, encounter
 from graph import foe_map
-
 
 
 def hero_health(self):
@@ -21,75 +20,77 @@ def hero_health_change(self, h_change):
         """
             exit(1)
     return new
+ 
 
 def foe_health_change(self, current_space, foe, f_change):
     gen_foe = foe_map[current_space]
     old_val = Hit_Points[gen_foe][foe]
     new_val = old_val + f_change
     Hit_Points[gen_foe][foe] = new_val
-    print Hit_Points[gen_foe][foe]
-    
-
+    return new_val
   
 def choose_attack(self, current_space):
     foe = encounter[current_space]
-    chooser = random.choice(foe)
-    return chooser          
+    foe_group = random.choice(foe)
+    return foe_group          
 
-def defense_input(self, foe):
+def defense_input(self, current_space, foe):
     user = raw_input("> ")    
     
     if 'hit' in user:
         f_change = -10
-        update_line = "Your foes health is now %s." % foe_health_change('self', foe, f_change)
+        update_line = "Your foes health is now %s." % foe_health_change('self', current_space, foe, f_change)
         return update_line
 
-
+    if 'scream' in user:
+        return "scream"
     else:
-         return "no hit"
+        not_found = "You're not even sure of what you were trying to do as you floundered to save your skin." 
+        return not_found   
       
 
 def attack_engine(self, current_space):
     val = 0  
+    gen_foe = foe_map[current_space]
     chooser = choose_attack('self', current_space)
-    group = random.choice(chooser[2:5])
-    h_change = sum(group[0:1])
-    #f_change = 
     lucky_line = "Luck favors you and the %s attack misses completely." % (str(chooser[0:1]).strip("[]")).strip("'")
     lucky = str(chooser[1:2]), lucky_line
-    first_attack = str(chooser[1:2]), str(group[1:2])
-    while hero_health('self') > 0:
-        #foe_health = 
+    
+    ###While hero is alive, while foe is alive, if first time in, if subsequent times in.
+    while hero_health('self') > 0: 
         foe = (str(chooser[0:1]).strip("[]")).strip("'")
-        group = random.choice(chooser[2:5])
-        subsequent_attacks = str(group[1:2])
-        hero_update = "Your health is now %s." % hero_health_change('self', h_change)
-
-        if foe_health > 0:
+        
+        while Hit_Points[gen_foe][foe] > 0:
             roll = random.randint(0, 4)
-            if roll != 1 and val == 0:   #should just update here
-                print str(chooser[1:2]), defense_input('self', foe)
-                val = 1
-            elif roll != 1 and val == 1:
-                print subsequent_attacks, hero_update, defense_input('self', foe)
-                val = 1       
-            elif roll == 1 and val == 0:
-                print lucky, defense_input('self', foe)
-                val = 1
-            elif roll == 1 and val == 1:
-                print lucky_line, defense_input('self', foe)
-                val = 1
+            chosen_attack = random.choice(chooser[2:5])
 
-        else:
-            print "Your foe perishes at your feet. At surviving, you are victorious."
-            #self.scene()
+            if val == 0:
+                print str(chooser[1:2])
+                
+                if roll != 1:
+                    print defense_input('self', current_space, foe)                                                                                                                                                                      
+                    val = 1    
+                elif roll == 1:
+                    print lucky, defense_input('self', current_space, foe)
+                    val = 1
 
-       
+            elif val == 1:
+
+                if roll == 1 and val == 1:
+                    print lucky_line, defense_input('self', current_space, foe)
+                    val = 1
+                elif roll != 1:
+                    h_change = sum(chosen_attack[0:1])
+                    print chosen_attack[1:2], "Your health is now %s." % hero_health_change('self', h_change), defense_input('self', current_space, foe)
+                    val = 1
+
+
+        print "Your foe perishes at your feet. At surviving, you are victorious."
+        #self.scene()
+
     print "With your last breath you scream \"Arturia!\" then you breath no more."
     exit(1)            
  
 current_space = 4 
-f_change = -10
 
-foe = 'red_dragon'
-go = foe_health_change('stuff', current_space, foe, f_change)    
+go = attack_engine('stuff', current_space)    
