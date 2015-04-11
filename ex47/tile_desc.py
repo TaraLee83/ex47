a2c8e733b3abbc5640146c200cd60fec528b1fea
Tara Habersack <taraleehabersack@gmail.com>
2015-04-10 19:22:04 -0700

    Added text for several tiles

--- ex47/tile_desc.py
+++ ex47/tile_desc.py
@@ -1,8 +1,16 @@
 #Tile descriptions
 
-scene = {1: """meadow desc""",
-         2: 'meadow desc',
-         3: 'meadow desc',
+scene = {1: "The meadow breaks and drops to North and West, ringed and "
+"buttressed by the jagged black rock drop into the sea. Here, where the "
+"sea spray travels the winds and falls upon the grasses, no trees take "
+"root. Only wheaten grasses grow in abundance, they sway under the gales "
+"in ripples that make the North-West meadow seem a sea of its own.",
+         2: "Clouds seldom trouble the skies so near to the meadows "
+"farthest edge. Hundreds of feet above the crashing sea, this stretch of "
+"land"         ,
+         3: "A fog hangs over the line of foothills that run from North "
+"to South. Prairie grass and wildflowers give way to thickening brush "
+"and a scattering of willows with arms outreached. "         ,
          4: """The Western range of summits, passable, unlike
 	    it's Eastern echo, stretched from Southwest to 
 	    Northwest. It ended at the point between the Eastern and
@@ -18,9 +26,18 @@ scene = {1: """meadow desc""",
 	    rock. The caves were crudley smashed into the rockface.
 	    Their entrances are all obscured by fires black.
 	        """,
-         6: 'dragonlands desc',
-         7: 'meadow desc',
-         8: 'meadow desc',
+         6: "A towering volcano churns. Spewing acidic fog out over "
+"itself. Twisted, grey trees spot the middle of the volcano and the "
+"base is thick with thorned brush and alive with the hissing of the "
+"volcano lizards that thrived in the shadow of the sulfuric chimney "
+"that kept a steady stream of avian meat falling their way.",
+         7: "A strip of wind-swept meadow runs from South to North. "
+"To your West this drops abruptly into the ocean. To the East is the "
+"back of a hut-like dwelling. Vines cover the sunburned walls. The "
+"plaster has fallen away in places to reveal cerulean blue brick. "
+"Ivy covers the back wall. Through the window you hear humming. The "
+"tune sounds faintly familiar. "         ,
+         8: "Meadow Mystic",
          9: 'meadow desc',
          10: """The Western summits flank your left. The wise and the 
 	     greedy both know to keep ones eyes trained on this sight.
@@ -56,9 +73,9 @@ scene = {1: """meadow desc""",
 	     onto death, these criminals, these unfortunate, these living
 	     beings, who once loved and hoped and wished quietly under the
 	     sky for peace within their hearts. You must keep moving. 
-	     There is a madness here as alive with intentions as a snake 
+	     There is a madness here as alive with intent as a snake 
 	     beneath the water.
-	     """
+	     """,
          12: 'dragonlands desc',
          13: 'meadow desc',
          14: 'meadow desc',
@@ -66,9 +83,9 @@ scene = {1: """meadow desc""",
          16: 'dragonlands desc',
          17: 'dragonlands desc',
          18: 'dragonlands desc',
-         19: 'desert desc',
-         20: 'desert desc',
-         21: 'desert desc',
+         19: 'broken_desert',
+         20: 'broken_desert',
+         21: 'broken_desert',
          22: 'forest desc',
          23: 'forest desc',
          24: 'forest desc',
@@ -86,18 +103,19 @@ scene = {1: """meadow desc""",
 	     weapons a so terribly talented glasssmith could 
 	     produce.
 	     """,
-         26: 'desert desc',
-         27: 'desert desc',
-         28: 'forest desc',
+         26: 'broken_desert',
+         27: 'broken_desert',
+         28: """You arrive atop a rocky outlook that provides
+         a fine view in every direction.""",
          29: 'entrance to Garzhed',
          30: 'Two possible encounters, a healer and the blacksmith',
          31: """Hills of green and mustard-yellow glass-shards
 	     stretched from the North-West, cave riddled rock-face
 	     to the North-East, where they abruptly curved due 
 	     North.
-	     """
-         32: 'desert desc',
-         33: 'desert desc',
+	     """,
+         32: 'broken_desert',
+         33: 'broken_desert',
          34: 'landing rock',
          35: 'forest desc',
          36: """Over the impenetrable trees to the North you 
