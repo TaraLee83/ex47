f774663f2e356090eb8ed91ba45f3ec9f757e1de
Tara Habersack <taraleehabersack@gmail.com>
2015-04-29 13:03:30 -0700

    text formatting function

--- ex47/format_incoming.py
+++ ex47/format_incoming.py
@@ -1,13 +1,17 @@
 import textwrap
+from library import *
 
 
+incoming = Desert_Scholar_Scenes[3]
 
-a_list = ["""You realized that you stood, staring into the millions of facets of those
-eyes. They shimmered and dissected the bleak world around into beautiful
-reflections.
-"""]
 
-ready_to_print = textwrap.dedent(a_list[0]).strip()
-for width in [80]:
-    print
-    print textwrap.fill(ready_to_print, initial_indent='     ', subsequent_indent='', width=width)
+def format_text(self, incoming):
+	string = (", ".join(incoming))
+	ready_to_print = textwrap.dedent(string).strip()
+	for width in [80]:
+		print
+    	print textwrap.fill(ready_to_print, initial_indent='     ', subsequent_indent='', width=width)
+    
+
+go = format_text('stuff', incoming)
+	   
