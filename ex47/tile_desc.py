10fb35095c036f93522208107d14d21e19520fec
Tara Habersack <taraleehabersack@gmail.com>
2015-05-03 14:18:13 -0700

    desc 11-20

--- ex47/tile_desc.py
+++ ex47/tile_desc.py
@@ -9,15 +9,16 @@ scene = {1: "The meadow breaks and drops to North and West, ringed and "
 "farthest edge. Hundreds of feet above the crashing sea, this stretch of "
 "land is clear of trees and home to crab grass and wild rose bushes."         ,
          3: "A fog hangs over the line of foothills that run from North "
-"to South. Prairie grass and wildflowers give way to thickening brush "
+"to South on the Eastern side. Prairie grass and wildflowers give way to thickening brush "
 "and a scattering of willows with arms outreached. "         ,
-         4: "The Western range of summits, passable, unlike it's Eastern "
-"echo, stretched from Southwest to Northwest. It ended at the point "
-"between the Eastern and Western summits known as The Dragons Smile. The "
-"Smile: three crooked lines of jagged rock outcropping that the cruel "
-"and ceaseless winds had filed into spear-like points. If the warm blue "
-"sky brought hope to this place, The Smile tore it apart and consumed it "
-"with delight.",
+         4: "A fog hangs over the line of foothills that run from North "
+"to South on the Western side. The low hills climbed quickly to meet The "
+"Smile. The Smile was three crooked lines of jagged rock outcropping that "
+"began at the upsweep of the foothills that divided the Northeast from "
+"the Northwest. It stretched the Northernmost sky to the foot of A Vumu. The cruel and "
+"ceaseless winds had filed house-sized boulders into spear-like points. "
+"If the warm blue sky brought hope to this place, The Smile tore it apart "
+"and consumed it with delight.",
          5: "The waters of The Sorrow were icy as befitting its surroundings. "
 "To the North, a dozen round openings in the three rows of jagged rock. The "
 "caves had been crudley smashed into the rockface. Their entrances are all "
@@ -64,25 +65,21 @@ scene = {1: "The meadow breaks and drops to North and West, ringed and "
 "over its brow the join of Eastern and Western Summits known as The "
 "Dragons Smile. Here in the bottom of The Sorrow that souls honey "
 "hardened into common gold.",
-         11: """You stand upon the ice encrusted string of rocks that
-	     make an island of these crystal encased soul shards. The 
-	     brilliance and beauty threatens to overwhelm you. You wish
-	     all at once to weep, to fall to your knees in thanks to the 
-	     gods that so small a soul could, in life, relieve you eyes
-	     with such beauty, to scream at the impossiblity of life at
-	     the cruelty of existence to give humans such detailed 
-	     comprehension at the deformity of their own souls. But mostly
-	     to smash, though no mortal ever could. To set free, even 
-	     onto death, these criminals, these unfortunate, these living
-	     beings, who once loved and hoped and wished quietly under the
-	     sky for peace within their hearts. You must keep moving. 
-	     There is a madness here as alive with intent as a snake 
-	     beneath the water.
-	     """,
-         12: "The Spine, a crooked and twisted ridge that took slow "
-"descent from the volcano down to the Eastern point of Estill, the "
-""         
-""        ,
+         11: "You stand upon the ice encrusted string of rocks that make "
+"an island of these crystal encased soul shards. The brilliance and beauty "
+"threatens to overwhelm you. You wish all at once to weep, to fall to your "
+"knees in thanks to the gods that your small soul could, in life, releive "
+"your eyes with such beauty, to scream at the impossiblity of life at the "
+"cruelty of existence to give humans such detailed comprehension at the "
+"deformity of their own souls. But mostly to smash, though no mortal ever "
+"could. To set free, even onto death, these criminals, these unfortunate, "
+"these living beings, who once loved and hoped and wished quietly under "
+"the sky for peace within their hearts. You must keep moving. There is a "
+"madness here as alive with intent as a snake beneath the water.",
+         12: "To the East was The Spine; a crooked ridge that twisted in "
+"gradual descent from A Vumu to Estill, Northernmost point of the "
+"Eastern hillocks. North, A Vumu melted into The Smile. West lay the "
+"terrible beauty of the soul shards. South lay more forest.",
          13: "The Northern sweep of cliffs climbs up out of the waters "
 "shadowing black volcanic rock, sheer and deadly beneath. Grasses become "
 "stubbled, then sparse, then barely a green or brown shoot to be seen, the "
@@ -93,13 +90,38 @@ scene = {1: "The meadow breaks and drops to North and West, ringed and "
 "Arturia? That unsettled you. Why had you known the name of this sea? And "
 "what sort of nonsense could fill you with such anger at something as "
 "impartial as the sea itself?",
-         14: 'meadow desc',
-         15: 'meadow desc',
-         16: 'dragonlands desc',
-         17: 'dragonlands desc',
-         18: 'dragonlands desc',
-         19: 'broken_desert',
-         20: 'broken_desert',
+         14: "To the North lay the meadow. Mostly flat lands covered by tall "
+"wheaten grasses. Patches of bright flowers grew, shaded beneath arching Oaks. "
+"To East and West, the modest beauty of the meadow gave way, quickly, to the "
+"starkness of the desert. South, sand, heating rising in waves off of it. But "
+"the sand seemed restless. It seemed to shift here or there even as you glanced "
+"over it.",
+         15: "To the North lay the meadow. Mostly flat lands covered by tall "
+"wheaten grasses. To the East, the modest beauty of the meadow gave way, quickly, "
+"to the stark desert. The Desert stretched beyond your range of vision to the "
+"South. Foothills decend from the North here, worn away by wet strong winds off "
+"the North sea.",
+         16: "Here the The Sorrow becomes The Tears, the narrow waterfall that "
+"quenches the forests great thirst. North are the Dragon lands. South, at the bottom "
+"of the cliff is the forest. East beyond The Tears, boulders at the cliffs edge. "
+"To the west lay the meadow. The rush of The Tears is loud and its mists cool the "
+"air but the waterfall is on the other side of the lowest foothill.",
+         17: "Here the The Sorrow becomes The Tears, the narrow waterfall that "
+"quenches the forests great thirst. North are the Dragon lands. South, at the bottom "
+"of the cliff is the forest. East beyond The Tears, boulders line the cliffs edge. "
+"To the west are the big rocks buttressing the waterfall.",
+         18: "The woods thicken as they sweep up and away from the Southern cliff "
+"edge. The Sorrow flows from beneath the soul crystals to the Northwest becoming "
+"The Tears at cliffs edge.",
+         19: "No shelter could be found from the relentless sun. Even A Meer "
+"lapped gently, no winds drove her to shore. Though here and there the sands shifted,"
+"restless with the creatures at home here, the horizon was empty. All but for a jagged "
+"outcropping of rock to the South.",
+         20: "A gnarled cacti. Yet the rest of the world was washed umber. The white hot "
+"sun reflected off of the sand making the whole earth shimmer with washed umber. The sky "
+"and air glowed in its washed umber reflection. The brightness of it burned onto your eyes "
+"so that when you looked up to the sky you saw only washed umber. North, more sand. South, "
+"more sand. West, the sea. East, more sand. Sand, sand, sand. "
          21: 'broken_desert',
          22: 'forest desc',
          23: 'forest desc',
