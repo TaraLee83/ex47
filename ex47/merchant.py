4210cd187c27e46a78afeec88bd4606713dc224e
Tara Habersack <taraleehabersack@gmail.com>
2015-06-11 14:48:23 -0700

    Merchant functionality all working well.

--- ex47/merchant.py
+++ ex47/merchant.py
@@ -1,39 +1,13 @@
 ###Merchant functionality
-from sys import exit
-from bag import *
+
+from bag import bag_contents, bag_items
 from format_incoming import format_text
+from maps import bag_map
 from scenes import encounter, Merchant_Scenes
 
-Merchant_Inp = {'yes': 1, 'coin': 1, 'buy': 1, 'thanks': 2, 'goodbye': 2, 'sell': 0}
-
-def merchant_main(self):
-    #Print intro, take input, parse input, take input until goodbye key triggered.
-    key = 0
-    val = 0
-    times_through = 0
-    scenes = encounter[15]
-    print format_text('self', Merchant_Scenes[0][0])
-
-    user_input = raw_input("> ")
-    
-    while key != 2:
-        for keys in Merchant_Inp:
-            if user_input in keys and times_through == 0:
-                key = Merchant_Inp[user_input]
-                times_through = 1
-                val += 1
-                print format_text('self', Merchant_Scenes[0][0])
-                print format_text('self', Merchant_Scenes[0][key])
-            elif user_input in keys and times_through != 0:
-                key = Merchant_Inp[user_input]
-                print format_text('self', Merchant_Scenes[0][key])    
-            elif user_input not in keys and val == len(Merchant_Inp):
-                print "    I don't understand what you're looking for."
-    else:
-        print format_text('self', Merchant_Scenes[0][2])
-
+Merchant_Inp = {'yes': 1, 'coin': 1, 'buy': 1, 'thanks': 2, 'goodbye': 2, 'sell': 0, 'done': 3}
    
-def merchant_main_1(self):
+def merchant_func(self):
     #Print intro, take input, parse input, take input until goodbye key triggered.
     val = 0
     key = 0
@@ -41,66 +15,52 @@ def merchant_main_1(self):
     print format_text('self', Merchant_Scenes[0][0])
     
     while key != 2:
-        user_input = raw_input("> ")
+        u_i = raw_input("> ")
         for keys in Merchant_Inp:
-            if user_input in keys:
-                print 1
-                key = Merchant_Inp[user_input]
-
+            if keys in u_i:
+                key = Merchant_Inp[keys]
                 if key == 0:
-                    print 2
-                    sell('self')
+                    print sell('self')
+                    key = 2
                 elif key != 0:
-                    print 3
                     print format_text('self', Merchant_Scenes[0][key])  
-
-            elif user_input not in keys:
+            elif u_i not in keys:
                 val += 1
                 if val == len(Merchant_Inp):
                     print "    I don't understand what you're looking for."
                 else:
                     continue    
     else:
-        print format_text('self', Merchant_Scenes[0][2])
-        exit(1)                
-
-
+        return "    Goodbye" 
+        
 def sell(self):
-    global bag_contents
-    print "Oh, you have brought me something. Wonderful! And what do you have to sell?"
+    #Check whether or not input matches item dictionary. Count qt of item possessed, get item value.
+    #Add gold to bag, delete item stock.
+    val = 0
+    print "    Do you have something to sell? What is it?"
 
-    user_input = raw_input("> ")
+    u_i = raw_input("> ")
 
-    if "bluebells" in user_input and "bluebells" in bag_contents[2]:
-        item = "bluebells"
-        val = 5
-        qt = bag_contents[2].count("bluebells")
-    elif "cactus flower" in user_input and "cactus flower" in bag_contents[2]:
-        item = "cactus flower"
-        val = 5
-        qt = bag_contents[2].count("cactus flower")    
-    elif "honeysuckle" in user_input and "honeysuckle" in bag_contents[2]:
-        item = "honeysuckle"
-        val = 5
-        qt = bag_contents[2].count("honeysuckle")     
-    elif "obsidian" in user_input and "obsidian" in bag_contents[4]:
-        item = "obsidian"
-        val = 20
-        qt = bag_contents[4].count("obsidian")
-    elif "shells" in user_input and "shells" in bag_contents[4]:
-        item = "shells"
-        val = 20
-        qt = bag_contents[4].count("shells")
-    elif "spewer toxin" in user_input and "spewer toxin" in bag_contents[4]:
-        item = "spewer toxin"
-        val = 20
-        qt = bag_contents[4].count("spewer toxin")
-    else:
-        return "It looks like you don't have any of that to sell."
- 
-    bag_contents[2] = [i for i in bag_contents[2] if i != item]
-    bag_contents[4] = [i for i in bag_contents[4] if i != item]
-    bag_contents[3][1] = bag_contents[3][1] + (val * qt)
-    return "Here is %r gold for your %s." % ((val * qt), item)
+    if "no" in u_i or "done" in u_i:
+            return "    Nothing? Alright." 
 
-go = merchant_main_1('self')    
+    for i in bag_items:
+        if i in u_i:
+            sec = bag_map[i][0]
+            if i in bag_contents[sec]:
+                qt = bag_contents[sec].count(i)
+                vl = bag_map[i][1]
+                bag_contents[3][1] = (bag_contents[3][1]) + (vl * qt)
+                while i in bag_contents[sec]:
+                    bag_contents[sec].remove(i)
+                else:
+                    return "    Thank you for the %s. Here is %r gold for your troubles." % (i, (vl * qt))
+            else:
+                print "    It looks like you don't have that item."
+                sell('self') 
+        elif i not in u_i:
+            val += 1
+            if val != len(bag_items):
+                continue
+            elif val == len(bag_items):
+                return "    I am not interested in purchasing that item." 
