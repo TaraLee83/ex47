13652d45ecf38ec6bb07a9d8205903736ee67e0d
Tara Habersack <taraleehabersack@gmail.com>
2015-06-26 14:08:08 -0700

    working through sorrow lake functionality

--- ex47/engine.py
+++ ex47/engine.py
@@ -2,9 +2,16 @@ import random
 from sys import exit
 from tile_desc import scene
 from format_incoming import format_text
+from help_f import help_func
 from input_parsing import *
 from maps import graph, foe_map, tile_loc, gather_map, weapon_efficacy_map
-from scenes import *
+from merchant import sell, merchant_func
+from sorrow_lake import sorrow_lake
+from scenes import (Red_Dragon_Scenes, Green_Dragon_Scenes, Gold_Dragon_Scenes, Scorpian_Scenes,
+Spewer_Scenes, The_Sorrow_Lake_Scenes, The_Sorrow_Island_Scenes, Tree_Disease_Scenes, Twiggins_Scenes, 
+Twiggins_Swarm_Scenes, Blacksmith_Scenes, Herbalist_Scenes, Desert_Scholar_Scenes, Meadow_Mystic_Scenes,
+Memory_Nymphs_Scenes, Merchant_Scenes, Tile_Change_Scene, White_Dragon_Scenes, Winner)
+from sorrow_func import the_sorrow
 from bag import *
 from hp import *
 
@@ -22,36 +29,42 @@ class Game(object):
         self.encounter_roll(current_space)
         
 ### Encounters    
-#MAKE SURE 23 IS ROLLING
     def encounter_roll(self, current_space):
-        p_e_list = (5, 8, 11, 15, 25, 29, 30)
+        i_e_list = (8, 23, 25, 29, 30)
         poison_list = (20, 21, 26, 27, 28, 35, 35, 36)
         x = random.randint(1, 5) 
         y = 4
-        if current_space in p_e_list:
-            self.passive_encounter(current_space)
-        elif x == y and current_space in poison_list:
-            self.poison_encounter(current_space) 
-        elif x != y and current_space in poison_list:
-            self.user_input(current_space)      
-        elif x == y and 3 == current_space or 14 == current_space:
-            print "    A fog descended upon the land. It thickened like a soup. All "
-            print "around, the landscape vanished from view. A peculiar wind stirred, "
-            print "it blew in every direction at once. Suddenly a strong gust lifted "
-            print "you up, carried you off and set you down somewhere entirely different. "
-            print "The fog cleared. "
-            current_space = random.randint(1, 36)
-            self.scene(current_space)    
-        elif current_space == 22 or current_space == 24:
+        if current_space in i_e_list:
+            self.interactive_encounter(current_space)
+        elif current_space == 15:
+            print merchant_func('self')
             self.user_input(current_space)
-        elif x == y and current_space == 23:
-            self.memory_nymph(current_space) 
-        elif x != y and current_space == 23:
-            self.user_input(current_space)       
-        else:
-            self.attack_engine(current_space) 
-       
-           
+        elif current_space == 5:
+            print sorrow_lake('stuff') 
+            self.user_input(current_space)   
+        #elif current_space == 5 or current_space == 11:
+        #    a, b, c = the_sorrow('self', current_space)
+        #    print a 
+        #    print b 
+        #    print c
+        #    self.user_input(current_space)
+        elif current_space == 22 or current_space == 24:
+            self.user_input(current_space)         
+        elif x == y:
+            if current_space in poison_list:
+                self.poison_encounter(current_space)
+            elif 3 == current_space or 14 == current_space:
+                print format_text('self', Tile_Change_Scene[0])
+                current_space = random.randint(1, 36)
+                self.scene(current_space)
+            elif current_space == 23:
+                self.memory_nymph(current_space)
+            else:
+                self.attack_engine(current_space)    
+        elif x != y:
+            self.user_input(current_space)
+        
+
 ### When encounter is rolled on hostile square           
     def hero_health(self):
         hero_hp = Hit_Points['Hero']
@@ -94,31 +107,31 @@ class Game(object):
 
     def defense_input(self, current_space, foe):
         #CHECK FOR WEAPON BONUS, AUTO USE WEAPON/ INSTRUCTIONS FOR USE
-        user = raw_input("> ")
+        u_i = raw_input("> ")
         ###Check input and weapon availability, obtain damage total
-        if "throw" in user and "obsidian disk" in bag_contents[1]:
+        if "throw" in u_i and "obsidian disk" in bag_contents[1]:
             weapon = "obsidian disk"
-        elif "thrust" in user and "polearm" in bag_contents[1]:
+        elif "thrust" in u_i and "polearm" in bag_contents[1]:
             weapon = "polearm"
-        elif "stab" in user and "dagger" in bag_contents[1]:
+        elif "stab" in u_i and "dagger" in bag_contents[1]:
             weapon = "dagger"
-        elif "hit" in user:
+        elif "hit" in u_i:
             new_total = Hit_Points[foe] - 5
             Hit_Points[foe] = (Hit_Points[foe] - 5)
             return "    You punch your foe. Your foes health is now %r." % new_total
         else:
             weapon = 0
 
-        return self.defense_input_return(user, weapon, foe)    
+        return self.defense_input_return(u_i, weapon, foe)    
 
-    def defense_input_return(self, user, weapon, foe):            
+    def defense_input_return(self, u_i, weapon, foe):            
         if weapon == 0:  
             return "    You flounder and lose ground."  
         elif weapon != 0:
             if self.weapon_check(weapon, foe) == True:
                 new_total = Hit_Points[foe] + self.attack_totalling(weapon, foe)
                 Hit_Points[foe] = new_total
-                return "    You %s your %s. Your foes health is now %r." % (user, weapon, new_total)
+                return "    You %s your %s. Your foes health is now %r." % (u_i, weapon, new_total)
 
             elif self.weapon_check(weapon, foe) == False:
                 return "    This weapon is useless against your foe."          
@@ -168,22 +181,36 @@ class Game(object):
         print "    With your last breath you scream \"Arturia!\" then you breath no more."
         exit(1)     
 
-    def passive_encounter(self, current_space):
+    def interactive_encounter(self, current_space):
         ###print intro scene, take input until exit scene triggered then pass to self.scene(current_space).
+        #Merchant_Inp
         current_dict = encounter_directory[current_space]
-        current_scene = encounter[current_space][0]
-        print format_text('self', current_scene)
-        value = 1
+        current_scenes = encounter[current_space]
+        intro_scene = current_scenes[0]
+        print format_text('self', intro_scene)
+        value = 0
+        counter = 0
 
         while value != 2:
-            user = raw_input("> ")
+            u_i = raw_input("> ")
             for key in current_dict:
-                if key in user:
+                if key in u_i:
                     value = current_dict[key]
-                    current_scene = encounter[current_space][current_dict[key]]
+                    current_scene = current_scenes[value] 
                     print format_text('self', current_scene)
-
+                elif key not in u_i:
+                    counter += 1
+                    if counter == len(current_dict):
+                        print "I don't understand."
+                    else:
+                        continue    
+        print format_text('self', scene[current_space])
         self.user_input(current_space)
+
+    def passive_encounter(self, current_space):
+        current_scenes = encounter[current_space]
+        intro_scene = current_scenes[0][0]
+        print format_text('self', intro_scene)    
 
     def poison_encounter(self, current_space):
         scene_set = encounter[current_space]
@@ -196,12 +223,12 @@ class Game(object):
         scenes = Memory_Nymphs_Scenes[1], Memory_Nymphs_Scenes[2]
         scene = random.choice(scenes)
         item = scene[1]
-        user_input = raw_input("> ")
-        if "sit" in user_input or "yes" in user_input or "talk" in user_input:
+        u_i = raw_input("> ")
+        if "sit" in u_i or "yes" in u_i or "talk" in u_i:
             print format_text("self", scene[2])
             print del_from_bag("stuff", item)
             self.user_input(current_space)            
-        elif "run" in user_input or "no" in user_input or "walk away" in user_input:
+        elif "run" in u_i or "no" in u_i or "walk away" in u_i:
             print "    You put some distance between yourself and the mysterious creature. "
             #Move player one space to the left.
             current_space = 22
@@ -222,7 +249,9 @@ class Game(object):
                 if subkey in user:
                     good_value = 1
                     self.point(subkey, current_space)
-
+        if "help" in user:
+            print help_func('self')
+            self.user_input(current_space)
         if good_value == 0:
             print "    I'm afraid I didn't understand your command. Please try again."
             self.user_input(current_space)
@@ -263,7 +292,7 @@ class Game(object):
 
 
 	    
-current_space = 4
+current_space = 5
 
 go = Game(current_space)
-go.scene(current_space)
+go.encounter_roll(current_space)
